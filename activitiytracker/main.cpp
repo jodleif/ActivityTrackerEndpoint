c83826f8782202abd4f6bf3565b681e026909e05
Jo Øivind Gjernes <jodleif@gmail.com>
2017-02-11 19:36:57 +0100

    Add exception handling to transaction.

--- activitiytracker/main.cpp
+++ activitiytracker/main.cpp
@@ -1,23 +1,26 @@
+#include "postgres.h"
 #include <QCoreApplication>
+#include <QHash>
+#include <iostream>
 #include <qhttpserver.hpp>
 #include <qhttpserverrequest.hpp>
 #include <qhttpserverresponse.hpp>
-#include <QHash>
-#include "postgres.h"
-#include <iostream>
-void printstuff(const QByteArray& arr) {
-    for(const auto& b : arr) std::cout << b;
-    std::cout << std::endl;
+void
+printstuff(const QByteArray& arr)
+{
+  for (const auto& b : arr)
+    std::cout << b;
+  std::cout << std::endl;
 }
 
 int
 main(int argc, char* argv[])
 {
-        auto ptr = db::open_db_connection();
-    {
-    db::make_transaction(ptr.get(),"dbname=rest dbhost=localhost username=jo password=123123123");
-    db::make_transaction(ptr.get(),"");
-    }
+  auto ptr = db::open_db_connection();
+  {
+    db::make_transaction(ptr.get(), "select * from activity");
+    db::make_transaction(ptr.get(), "select * from users");
+  }
   QCoreApplication app(argc, argv);
   qhttp::server::QHttpServer server(&app);
   server.listen(QHostAddress::Any, 8080, [](qhttp::server::QHttpRequest* req,
@@ -25,8 +28,8 @@ main(int argc, char* argv[])
     req->collectData();
     printstuff(req->collectedData());
     auto x = req->headers();
-    for(auto& ele : x){
-        printstuff(ele);
+    for (auto& ele : x) {
+      printstuff(ele);
     }
     req->onEnd([req, res]() {
       res->setStatusCode(qhttp::ESTATUS_OK);
--- activitiytracker/postgres.cpp
+++ activitiytracker/postgres.cpp
@@ -1,26 +1,33 @@
 #include "postgres.h"
-#include <iostream>
 #include <cassert>
+#include <iostream>
 
-void db::make_transaction(pqxx::connection * ptr,std::string sql_query)
+void
+db::make_transaction(pqxx::connection* ptr, std::string sql_query)
 {
-    assert(ptr);
-    pqxx::work transaction(*ptr);
-    auto r = transaction.exec("select * from activity");
-    if(r.size() == 0) return;
+  assert(ptr);
+  assert(sql_query.size() > 0);
+  pqxx::work transaction(*ptr);
+  try {
+    auto r = transaction.exec(sql_query);
+    if (r.size() == 0)
+      return;
     std::cout << "Got " << r.size() << " results\n";
-    for(const auto& row : r){
-        for(const auto& field : row){
-            std::cout << field.name() << ":" << field.c_str() << "\t";
-        }
-        std::cout << '\n';
+    for (const auto& row : r) {
+      for (const auto& field : row) {
+        std::cout << field.name() << ":" << field.c_str() << "\t";
+      }
+      std::cout << '\n';
     }
-
+  } catch (std::exception& e) {
+    std::cerr << "Failed processing: " << sql_query << '\n'
+              << e.what() << std::endl;
+  }
 }
 
 /**
  * @brief db::open_db_connection
- * Will open a new database-connection based on the following shell-variables
+ * Will open a new database-connection based on the following env-variables
  *  PGDATABASE  (name of database; defaults to your user name)
  *  PGHOST      (database server; defaults to local machine)
  *  PGPORT      (TCP port to connect to; default is 5432)
@@ -28,14 +35,13 @@ void db::make_transaction(pqxx::connection * ptr,std::string sql_query)
  *  PGPASSWORD  (your PostgreSQL password, if needed)
  * @return unique_ptr with db-handle
  */
-std::unique_ptr<pqxx::connection> db::open_db_connection()
+std::unique_ptr<pqxx::connection>
+db::open_db_connection()
 {
-    try {
+  try {
     return std::unique_ptr<pqxx::connection>(new pqxx::connection());
-    } catch (std::exception & e) {
-        std::cerr << "Failed connecting to DB\n"
-                  << e.what()
-                  << std::endl;
-    }
-    return std::unique_ptr<pqxx::connection>(nullptr);
+  } catch (std::exception& e) {
+    std::cerr << "Failed connecting to DB\n" << e.what() << std::endl;
+  }
+  return std::unique_ptr<pqxx::connection>(nullptr);
 }
